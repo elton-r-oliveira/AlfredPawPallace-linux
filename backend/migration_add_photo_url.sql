d5f46b6f050dca29c1a754aa9b827d4f36e0a9dc
Elton R. Oliveira <126191399+elton-r-oliveira@users.noreply.github.com>
2026-05-31 23:49:18 -0300

    .

--- backend/migration_add_photo_url.sql
+++
@@ -1,7 +0,0 @@
--- Migration: adiciona coluna photo_url na tabela t_pets
--- Executar no banco BDAlfredPawPalace antes de subir o novo backend
-
-USE BDAlfredPawPalace;
-
-ALTER TABLE t_pets
-  ADD COLUMN photo_url VARCHAR(500) NULL AFTER animal_type;
